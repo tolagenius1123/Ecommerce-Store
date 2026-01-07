8bb1a6ecb8fe24d35b4510a9800c3dfa1636eb0e
Omotola Jinadu <91120863+tolagenius1123@users.noreply.github.com>
2026-01-07 17:50:31 +0100

    add webhook for paystripe payment

--- src/actions/createCheckoutSession.ts
+++ src/actions/createCheckoutSession.ts
@@ -36,14 +36,22 @@ export async function createCheckoutSession(
 			customerId = customers.data[0].id;
 		}
 
+		const baseUrl =
+			process.env.NODE_ENV === "production"
+				? `https://${process.env.VERCEL_URL}`
+				: `${process.env.NEXT_PUBLIC_BASE_URL}`;
+
+		const successUrl = `${baseUrl}/success?session_id={CHECKOUT_SESSION_ID}&orderNumber=${metadata.orderNumber}`;
+		const cancelUrl = `${baseUrl}/basket`;
+
 		const session = await stripe.checkout.sessions.create({
 			customer: customerId,
 			customer_creation: customerId ? undefined : "always",
 			metadata,
 			mode: "payment",
 			allow_promotion_codes: true,
-			success_url: `${`https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}&orderNumber=${metadata.orderNumber}`,
-			cancel_url: `${`https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL}/basket`,
+			success_url: successUrl,
+			cancel_url: cancelUrl,
 			line_items: items.map((item) => ({
 				price_data: {
 					currency: "usd",
